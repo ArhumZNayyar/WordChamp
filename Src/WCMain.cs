c62c61e80134eaf0378608718cdebd7369190fca
Arhum Z. Nayyar <47288337+ArhumZNayyar@users.noreply.github.com>
2022-03-01 13:04:33 -0500

    Initialize Grid, Create Graphics

--- Src/WCMain.cs
+++ Src/WCMain.cs
@@ -1,10 +1,31 @@
+using WordChamp.Src;
+
 namespace WordChamp
 {
     public partial class WCMain : Form
     {
+        Graphics graphics;
+        Grid WCGrid = new Grid();
+        string word = "null";
+
         public WCMain()
         {
             InitializeComponent();
+            InitializeGrid(WCGrid);
+            graphics = this.CreateGraphics();
+        }
+
+        public void InitializeGrid(Grid grid)
+        {
+            WCGrid.maxColumns = word.Length;
+            WCGrid.word = this.word;
+            for (int row = 1; row <= grid.maxRows; row++)
+            {
+                for (int col = 1; col <= grid.maxColumns; col++)
+                {
+                    grid.createNode(col, row, grid);
+                }
+            }
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
