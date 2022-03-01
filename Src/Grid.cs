6342722c386b9dad8b18325a5054b144883e82fd
Arhum Z. Nayyar <47288337+ArhumZNayyar@users.noreply.github.com>
2022-03-01 12:52:58 -0500

    Create Grid.cs
    Grid class will store the nodes where each node represents a character in the answer string.

---
+++ Src/Grid.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordChamp.Src
+{
+    public class Grid
+    {
+        public int maxRows = 5;
+        public int maxColumns = 5;
+        public int amountofNodes = 25;
+        public string word = "null";
+        public List<Node> allNodes = new List<Node>();
+        public Grid.Node? Nodes;
+
+        public class Node : Grid
+        {
+            /* Node Data */
+            public int rowPos;
+            public int colPos;
+            public int x;
+            public int y;
+            public char userAnswer;
+            public char trueAnswer;
+            public bool correct;
+            public Color color;
+
+            public Node()
+            {
+                this.rowPos = 0;
+                this.colPos = 0;
+                this.x = 0;
+                this.y = 0;
+                this.color = Color.Black;
+                this.correct = false;
+                this.trueAnswer = 'a';
+            }
+        }
+    }
+}
